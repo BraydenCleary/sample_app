30eb6c9c4edab34bba20dfa35312349d0b90ec54
Brayden Cleary <brayden.cleary@gmail.com>
2012-07-22 21:47:51 -0500

    Progress

---
+++ db/migrate/20120723023109_recreate_user_table.rb
@@ -0,0 +1,11 @@
+class RecreateUserTable < ActiveRecord::Migration
+
+	create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
+  end
+
+end
